c87263e939e7bd79706c74285b5abf8cbea5a18c
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2021-11-05 19:41:46 +0530

    WIP:adding function method for resolving failing test

--- test/GovernanceStaking.js
+++ test/GovernanceStaking.js
@@ -4,7 +4,7 @@ contract('GovernanceStaking' , (accounts) => {
 it("is able to return 3" , async() => {
 
 const GovernanceStakingInstance = await GovernanceStaking.deployed();
-assert.equal(await(GovernanceStakingInstance.func1()) , 3); 
+assert.equal(await(GovernanceStakingInstance.func1().call()) , 3); 
 
     
 });
